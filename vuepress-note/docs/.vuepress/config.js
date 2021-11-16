51f730f9a96684645c94b94ff8023a7a543249e6
韩立涛 <hanlitao@daojia-inc.com>
2021-11-16 14:26:14 +0800

    修改
    修改

--- vuepress-note/docs/.vuepress/config.js
+++ vuepress-note/docs/.vuepress/config.js
@@ -24,6 +24,29 @@ module.exports = config({
       "script",
       { src: "https://cdn.jsdelivr.net/npm/@babel/standalone/babel.min.js" },
     ],
+    ['script', {}, `
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?76592c45133291700273e2e5ea3f89b3";
+              var s = document.getElementsByTagName("script")[0]; 
+              s.parentNode.insertBefore(hm, s);
+            })();
+
+            // 引入谷歌,不需要可删除这段
+            var hm1 = document.createElement("script");
+            hm1.src = "https://www.googletagmanager.com/gtag/js?id=G-9F64JQSDVD";
+            var s1 = document.getElementsByTagName("script")[0]; 
+            s1.parentNode.insertBefore(hm1, s1);
+            })();
+
+            // 谷歌加载,不需要可删除
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            // 衡量ID
+            gtag('config', 'G-9F64JQSDVD');
+        `]
   ],
 
   locales: {
@@ -170,7 +193,7 @@ module.exports = config({
     [
       '@vuepress/google-analytics',
       {
-        ga: 'G-9F64JQSDVD' // UA-00000000-0
+        'ga': 'G-9F64JQSDVD' // UA-00000000-0
       }
     ]
   ]
